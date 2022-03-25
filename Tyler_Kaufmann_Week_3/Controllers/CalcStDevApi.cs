1a5a4cb84291872d72a08aa20f16a07687f5de47
Terribly-Shady-Guy <tylerkaufmann98@gmail.com>
2022-03-24 22:26:12 -0500

    Minor code clean up

--- Tyler_Kaufmann_Week_3/Controllers/CalcStDevApi.cs
+++ Tyler_Kaufmann_Week_3/Controllers/CalcStDevApi.cs
@@ -11,12 +11,12 @@ namespace Tyler_Kaufmann_Week_3.Controllers
         public ActionResult<List<string>> PostStDev(List<int> data)
         {
             LogObject(data);
-            data = data.OrderBy(x => x).ToList();
+            data.Sort();
 
             List<int> childList = new List<int>();
             List<string> results = new List<string>();
 
-            foreach(int number in data)
+            foreach (int number in data)
             {
                 childList.Add(number);
                 int childLength = childList.Count;
@@ -35,17 +35,17 @@ namespace Tyler_Kaufmann_Week_3.Controllers
             return results;
         }
 
-        // Calculates Standard Deviation using Sqrt((sum((x[i] - sampleMean)^2)) / (N - 1)).
+        // Calculates Standard Deviation using Sqrt(Sum((x[i] - sampleMean)^2) / (N - 1)).
         private static double CalcStDev(List<int> data, int length)
         {
-            double sampleMean = data.Average(x => x);
-            double step2Result = data.Sum(num => Math.Pow(num - sampleMean, 2));
-            return Math.Sqrt(step2Result / (length - 1));
+            double sampleMean = data.Average();
+            double sum = data.Sum(num => Math.Pow(num - sampleMean, 2));
+            return Math.Sqrt(sum / (length - 1));
         }
 
-        private void LogObject(List<int> data)
+        private static void LogObject(List<int> data)
         {
-            foreach(int num in data)
+            foreach (int num in data)
             {
                 System.Diagnostics.Debug.WriteLine(num);
             }
